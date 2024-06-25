e7f39a32790e55410f748d5a18c51868476087fa
nhatdongdang <144138246+nhatdongdang@users.noreply.github.com>
2024-06-25 12:55:25 +1000

    Fix code style and cmake

--- src/main.c
+++ src/main.c
@@ -1,117 +1,113 @@
-#include <stdlib.h>
-#include <stdio.h>
 #include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 
-typedef struct{
+typedef struct {
     int rows;
     int cols;
     float** data;
-}Matrix;
+} matrix;
 
-Matrix* weight[7];
-Matrix* biase[7];
+matrix* weight[7];
+matrix* biase[7];
 
-Matrix* createMatrix(int rows, int cols) {
-    Matrix *matrix = (Matrix*)malloc(sizeof(Matrix));
-    matrix->rows = rows;
-    matrix->cols = cols;
-    matrix->data = (float**)malloc(rows *  sizeof(float*));
+matrix* createMatrix(int rows, int cols) {
+    matrix* res = (matrix*)malloc(sizeof(matrix));
+    res->rows = rows;
+    res->cols = cols;
+    res->data = (float**)malloc(rows * sizeof(float*));
     for (int i = 0; i < rows; i++) {
-        matrix->data[i] = (float*)malloc(cols * sizeof(float));
+        res->data[i] = (float*)malloc(cols * sizeof(float));
     }
-    return matrix;
+    return res;
 }
 
-Matrix* multiplyMatrices(const Matrix *a, const Matrix *b) {
-    Matrix* result = createMatrix(a->rows, b->cols);
+void multiplyMatrices(const matrix* a, const matrix* b, const matrix* result) {
     for (int i = 0; i < result->rows; i++) {
         for (int j = 0; j < result->cols; j++) {
             float sum = 0;
             for (int k = 0; k < a->cols; k++) {
-                sum+=(a->data)[i][k]*((b->data)[k][j]);
+                sum += (a->data)[i][k] * ((b->data)[k][j]);
             }
             (result->data)[i][j] = sum;
         }
     }
-    return result;
 }
 
-void addMatrix(Matrix *a, const Matrix *b){
-    for (int i=0;i<a->rows;i++){
-        for (int j=0;j<a->cols;j++){
-            (a->data)[i][j]+=(b->data)[i][j];
+void addMatrix(matrix* a, const matrix* b) {
+    for (int i = 0; i < a->rows; i++) {
+        for (int j = 0; j < a->cols; j++) {
+            (a->data)[i][j] += (b->data)[i][j];
         }
     }
 }
 
-void ReLU(Matrix* a){
-    for (int i=0;i<a->rows;i++){
-        for (int j=0;j<a->cols;j++){
-            if ((a->data)[i][j]<(float)0) (a->data)[i][j]=(float)0;
+void ReLU(matrix* a) {
+    for (int i = 0; i < a->rows; i++) {
+        for (int j = 0; j < a->cols; j++) {
+            if ((a->data)[i][j] < (float)0)
+                (a->data)[i][j] = (float)0;
         }
     }
 }
 
-void softmax(Matrix* a){
-    float res=(float)0;
-    for (int i=0;i<a->rows;i++){
-        for (int j=0;j<a->cols;j++){
-            res+=exp((a->data)[i][j]);
+void softmax(matrix* a) {
+    float res = (float)0;
+    for (int i = 0; i < a->rows; i++) {
+        for (int j = 0; j < a->cols; j++) {
+            res += exp((a->data)[i][j]);
         }
     }
-    for (int i=0;i<a->rows;i++){
-        for (int j=0;j<a->cols;j++){
-            (a->data)[i][j]/=res;
+    for (int i = 0; i < a->rows; i++) {
+        for (int j = 0; j < a->cols; j++) {
+            (a->data)[i][j] /= res;
         }
     }
 }
 
-
-void processWeight(char* line,int layer) {
+void processWeight(char* line, int layer) {
     char* token;
     float value;
-    const char *delimiter ="," ;
+    const char* delimiter = ",";
 
     token = strtok(line, delimiter);
-    for (int i=0;i<weight[layer]->rows;i++){
-        for (int j=0;j<weight[layer]->cols;j++){
-            value=strtof(token,NULL);
-            (weight[layer]->data)[i][j]=value;
-            token =strtok(NULL,delimiter);
+    for (int i = 0; i < weight[layer]->rows; i++) {
+        for (int j = 0; j < weight[layer]->cols; j++) {
+            value = strtof(token, NULL);
+            (weight[layer]->data)[i][j] = value;
+            token = strtok(NULL, delimiter);
         }
     }
 }
 
-void processBiase(char* line,int layer) {
+void processBiase(char* line, int layer) {
     char* token;
     float value;
-    const char *delimiter ="," ;
+    const char* delimiter = ",";
 
     token = strtok(line, delimiter);
-    
-    for (int i=0;i<biase[layer]->rows;i++){
-        value=strtof(token,NULL);
-        (biase[layer]->data)[i][0]=value;
-        token =strtok(NULL,delimiter);
-    }
 
+    for (int i = 0; i < biase[layer]->rows; i++) {
+        value = strtof(token, NULL);
+        (biase[layer]->data)[i][0] = value;
+        token = strtok(NULL, delimiter);
+    }
 }
 
+void readModel() {
+    FILE* file = fopen("../weights_and_biases.txt", "r");
 
-void readModel(){
-    FILE *file = fopen("../weights_and_biases.txt", "r");
-
-    char *line = NULL;
+    char* line = NULL;
     size_t len = 0;
     int line_number = 0;
-    int layer=0;
+    int layer = 0;
 
     while ((getline(&line, &len, file)) != -1) {
-        if ((line_number-1)%4==0){
-            processWeight(line,layer);
-        }else if ((line_number-3)%4 ==0){
-            processBiase(line,layer);
+        if ((line_number - 1) % 4 == 0) {
+            processWeight(line, layer);
+        } else if ((line_number - 3) % 4 == 0) {
+            processBiase(line, layer);
             layer++;
         }
         line_number++;
@@ -121,10 +117,10 @@ void readModel(){
     fclose(file);
 }
 
-void readInput(Matrix *a,char* fileName){
-    FILE *file = fopen(fileName, "r");
+void readInput(matrix* a, char* fileName) {
+    FILE* file = fopen(fileName, "r");
 
-    char *line = NULL;
+    char* line = NULL;
     size_t len = 0;
     ssize_t read;
     int line_number = 0;
@@ -132,62 +128,89 @@ void readInput(Matrix *a,char* fileName){
     getline(&line, &len, file);
     char* token;
     float value;
-    const char *delimiter ="," ;
+    const char* delimiter = ",";
     token = strtok(line, delimiter);
-    int size=0;
-    for (int i=0;i<225;i++){
-        value=strtof(token,NULL);
-        (a->data)[i][0]=value;
-        token =strtok(NULL,delimiter);
+    int size = 0;
+    for (int i = 0; i < 225; i++) {
+        value = strtof(token, NULL);
+        (a->data)[i][0] = value;
+        token = strtok(NULL, delimiter);
     }
     free(line);
     fclose(file);
-
 }
 
-Matrix* propagateForward(const Matrix* weight,const Matrix* input,const Matrix* biase){
-    Matrix* res=multiplyMatrices(weight,input);
-    addMatrix(res,biase);
-    return res;
+void propagateForward(const matrix* weight, const matrix* input, matrix* nextLayer, const matrix* biase) {
+    multiplyMatrices(weight, input, nextLayer);
+    addMatrix(nextLayer, biase);
 }
 
-int getResult(Matrix *a){
-    int idx=0;
-    float res=INT32_MIN;
-    for(int i=0;i<a->rows;i++){
-        if (res<(a->data)[i][0]){
-            res=(a->data)[i][0];
-            idx=i;
+int getResult(matrix* a) {
+    int idx = 0;
+    float res = INT32_MIN;
+    for (int i = 0; i < a->rows; i++) {
+        if (res < (a->data)[i][0]) {
+            res = (a->data)[i][0];
+            idx = i;
         }
     }
     return idx;
 }
 
-int main(){
-    
-    // Load model
-    weight[0]=createMatrix(98,225);
-    weight[1]=createMatrix(65,98);
-    weight[2]=createMatrix(50,65);
-    weight[3]=createMatrix(30,50);
-    weight[4]=createMatrix(25,30);
-    weight[5]=createMatrix(40,25);
-    weight[6]=createMatrix(52,40);
+int inference(matrix* input) {
+    matrix* layer[7];
+    layer[0] = createMatrix(98, 1);
+    layer[1] = createMatrix(65, 1);
+    layer[2] = createMatrix(50, 1);
+    layer[3] = createMatrix(30, 1);
+    layer[4] = createMatrix(25, 1);
+    layer[5] = createMatrix(40, 1);
+    layer[6] = createMatrix(52, 1);
 
-    biase[0]=createMatrix(98,1);
-    biase[1]=createMatrix(65,1);
-    biase[2]=createMatrix(50,1);
-    biase[3]=createMatrix(30,1);
-    biase[4]=createMatrix(25,1);
-    biase[5]=createMatrix(40,1);
-    biase[6]=createMatrix(52,1);
+    propagateForward(weight[0], input, layer[0], biase[0]);
+    ReLU(layer[0]);
+    propagateForward(weight[1], layer[0], layer[1], biase[1]);
+    ReLU(layer[1]);
+    propagateForward(weight[2], layer[1], layer[2], biase[2]);
+    ReLU(layer[2]);
+    propagateForward(weight[3], layer[2], layer[3], biase[3]);
+    ReLU(layer[3]);
+    propagateForward(weight[4], layer[3], layer[4], biase[4]);
+    ReLU(layer[4]);
+    propagateForward(weight[5], layer[4], layer[5], biase[5]);
+    ReLU(layer[5]);
+
+    propagateForward(weight[6], layer[5], layer[6], biase[6]);
+    softmax(layer[6]);
+    return getResult(layer[6]);
+}
+
+int main() {
+
+    // Load model
+    weight[0] = createMatrix(98, 225);
+    weight[1] = createMatrix(65, 98);
+    weight[2] = createMatrix(50, 65);
+    weight[3] = createMatrix(30, 50);
+    weight[4] = createMatrix(25, 30);
+    weight[5] = createMatrix(40, 25);
+    weight[6] = createMatrix(52, 40);
+
+    biase[0] = createMatrix(98, 1);
+    biase[1] = createMatrix(65, 1);
+    biase[2] = createMatrix(50, 1);
+    biase[3] = createMatrix(30, 1);
+    biase[4] = createMatrix(25, 1);
+    biase[5] = createMatrix(40, 1);
+    biase[6] = createMatrix(52, 1);
 
     readModel();
 
-
     char str[10];
-    char fileName[100]; 
-    for (int i=1;i<=52;i++){
+    char fileName[100];
+    matrix* input = createMatrix(255, 1);
+
+    for (int i = 1; i <= 52; i++) {
         if (i < 10) {
             sprintf(str, "0%d", i);
         } else {
@@ -198,23 +221,13 @@ int main(){
         strcat(fileName, "out.txt");
 
         // Read txt file
-        Matrix* input=createMatrix(255,1);
-        readInput(input ,fileName);
+        readInput(input, fileName);
 
-        // Inference process
-        for (int j=0;j<6;j++){
-            input=propagateForward(weight[j],input,biase[j]);
-            ReLU(input);
-        }
-        Matrix* output=propagateForward(weight[6],input,biase[6]);
-        softmax(output);
-        
         // Check result
-        if (getResult(output)+1==i){
-            printf("Test %d correct ✅\n",i);
-        }else{
-            printf("Test %d incorrect ❌\n",i);
+        if (inference(input) + 1 == i) {
+            printf("Test %d correct ✅\n", i);
+        } else {
+            printf("Test %d incorrect ❌\n", i);
         }
     }
-
 }
