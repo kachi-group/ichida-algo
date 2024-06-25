effb5df079c7d7f57f0c82e893a05c13178a8bbc
nhatdongdang <144138246+nhatdongdang@users.noreply.github.com>
2024-06-25 13:28:38 +1000

    Add O3 to cmake

---
+++ speed_demo_cpu.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+if [ "$#" -ne 2 ]; then
+    echo "Usage: speed_cpu <relative_path_to_weights_and_biases.txt> <relative_path_to_input_tensor_directory>"
+    exit 1
+fi
+
+weights_and_biases=$1
+input_tensor_dir=$2
+
+binary="speed_cpu"
+
+if [ ! -f "$binary" ]; then
+    echo "Binary $binary not found!"
+    exit 1
+fi
+
+start_time=$(date +%s)
+./$binary "$weights_and_biases" "$input_tensor_dir"
+
+end_time=$(date +%s)
+execution_time=$((end_time - start_time))
+
+if [ ! -f "results.csv" ]; then
+    echo "Error: results.csv not found!"
+    exit 1
+else
+    echo "results.csv found!"
+fi
+
+echo "Execution time: $execution_time seconds"
