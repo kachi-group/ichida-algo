d0ceff7b84a34f82f3315dbb54455410d2d5f3d7
nhatdongdang <144138246+nhatdongdang@users.noreply.github.com>
2024-06-27 00:05:44 +1000

    Optimize matrix multiplication using column major order

--- src/matrix.c
+++ src/matrix.c
@@ -1,57 +1,48 @@
 #include "../include/matrix.h"
 #include "math.h"
+#include "stdio.h"
 #include "stdlib.h"
 
 matrix* new_matrix(int rows, int cols) {
     matrix* res = (matrix*)malloc(sizeof(matrix));
     res->rows = rows;
     res->cols = cols;
-    res->data = (float**)malloc(rows * sizeof(float*));
-    for (int i = 0; i < rows; i++) {
-        res->data[i] = (float*)malloc(cols * sizeof(float));
-    }
+    res->data = (float*)malloc((rows * cols) * sizeof(float));
     return res;
 }
 
-void matrix_mul(const matrix* a, const matrix* b, const matrix* result) {
-    for (int i = 0; i < result->rows; i++) {
-        for (int j = 0; j < result->cols; j++) {
-            float sum = 0;
-            for (int k = 0; k < a->cols; k++) {
-                sum += (a->data)[i][k] * ((b->data)[k][j]);
-            }
-            (result->data)[i][j] = sum;
+void matrix_mul(const matrix* __restrict__ a, const matrix* __restrict__ b, const matrix* __restrict__ result) {
+    int m = result->rows;
+    int p = a->cols;
+    for (int i = 0; i < m; i++) {
+        float sum = 0;
+        int h = i * p;
+        for (int k = 0; k < p; k++) {
+            sum += (a->data)[h + k] * ((b->data)[k]);
         }
+        (result->data)[i] = sum;
     }
 }
 
 void matrix_add(matrix* a, const matrix* b) {
     for (int i = 0; i < a->rows; i++) {
-        for (int j = 0; j < a->cols; j++) {
-            (a->data)[i][j] += (b->data)[i][j];
-        }
+        (a->data)[i] += (b->data)[i];
     }
 }
 
 void relu(matrix* a) {
     for (int i = 0; i < a->rows; i++) {
-        for (int j = 0; j < a->cols; j++) {
-            if ((a->data)[i][j] < (float)0)
-                (a->data)[i][j] = (float)0;
-        }
+        if ((a->data)[i] < (float)0)
+            (a->data)[i] = (float)0;
     }
 }
 
 void softmax(matrix* a) {
     float res = (float)0;
     for (int i = 0; i < a->rows; i++) {
-        for (int j = 0; j < a->cols; j++) {
-            res += exp((a->data)[i][j]);
-        }
+        res += exp((a->data)[i]);
     }
     for (int i = 0; i < a->rows; i++) {
-        for (int j = 0; j < a->cols; j++) {
-            (a->data)[i][j] /= res;
-        }
+        (a->data)[i] /= res;
     }
 }
