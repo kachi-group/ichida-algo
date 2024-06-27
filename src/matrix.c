92e5c13d29b57e644f4cadcbe4ef146a7723f282
Johnathan Chan <jchan0867@gmail.com>
2024-06-27 16:48:24 +1000

    KAIOOOKEEEN

--- src/matrix.c
+++ src/matrix.c
@@ -1,4 +1,5 @@
 #include "../include/matrix.h"
+#include <immintrin.h>
 #include <math.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -70,11 +71,35 @@ void matrix_mul(const matrix* weights, const matrix* inputs, const matrix* __res
 //         (result->data)[i] = sum;
 //     }
 // }
-
+// addition using unaligned avx
 void matrix_add(matrix* a, const matrix* b) {
-    for (int i = 0; i < a->rows; i++) {
-        (a->data)[i] += (b->data)[i];
+    int total_size = a->rows * a->cols;
+    int i = 0;
+    float* a_data = a->data;
+    float* b_data = b->data;
+
+    // loop unroll 8
+    for (; i <= total_size - 8; i += 8) {
+        // load  data into avx register
+        __m256 a_vec = _mm256_loadu_ps(&a_data[i]);
+        __m256 b_vec = _mm256_loadu_ps(&b_data[i]);
+
+        // sum of them
+        __m256 result_vec = _mm256_add_ps(a_vec, b_vec);
+
+        // saved back to the array
+        _mm256_storeu_ps(&a_data[i], result_vec);
     }
+
+    // remaining to handle edge cases,
+    int remaining = total_size - i;
+    // masking is like if statement i.e remaining> 7, remaining > 6, etc
+    __m256i mask = _mm256_cmpgt_epi32(_mm256_set1_epi32(remaining), _mm256_set_epi32(7, 6, 5, 4, 3, 2, 1, 0));
+    // same shit
+    __m256 a_vec = _mm256_maskload_ps(&a_data[i], mask);
+    __m256 b_vec = _mm256_maskload_ps(&b_data[i], mask);
+    __m256 result_vec = _mm256_add_ps(a_vec, b_vec);
+    _mm256_maskstore_ps(&a_data[i], mask, result_vec);
 }
 
 void relu(matrix* a) {
